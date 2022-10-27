37c3d66165c34d9452ab673b3ab6d9a6f9afcbe9
olukinni_029 <olukinnif`150216029@gmail.com>
2022-10-27 10:44:36 +0100

    inital commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({
+    origin:"*"
+}));
+
+const user={
+    slackUsername:"Olukinni Feranmi",
+    backend:true,
+    age:"24",
+    bio:"Ambitious, Smart, Hardworking, Optimistic, Mentally, Socially, and Emotionally Capable, always ready to learn. I'm a passionate Software Development, trying to change and add to my community through the tech world."
+}
+
+const port = 3540;
+
+app.get("/",(req,res)=>{
+    res.send("Zuri First Task");
+});
+
+app.get("/user",(req,res)=>{
+res.send(user);
+});
+
+
+app.listen(port,()=>{
+    console.log(`Server is running on http://localhost:${port}`);
+});
