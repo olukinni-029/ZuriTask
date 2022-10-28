b993b4b182a924a30e9739a26e706c410fc6b912
olukinni_029 <olukinnif`150216029@gmail.com>
2022-10-28 14:42:45 +0100

    updated

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(cors({
 const user={
     "slackUsername":"OLUKINNI",
     "backend":true,
-    "age":"24",
+    "age":24,
     "bio":"Ambitious, Smart, Hardworking, Optimistic, Mentally, Socially, and Emotionally Capable, always ready to learn. I'm a passionate Software Development, trying to change and add to my community through the tech world."
 };
 
